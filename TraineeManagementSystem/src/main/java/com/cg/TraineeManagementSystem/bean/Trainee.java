aef7a3c20c6b989f60c9ea3723f91b3b1cedd74f
manishabukka <47662040+manishabukka@users.noreply.github.com>
2019-05-04 11:36:47 +0530

    Add files via upload

---
+++ TraineeManagementSystem/src/main/java/com/cg/TraineeManagementSystem/bean/Trainee.java
@@ -0,0 +1,41 @@
+package com.cg.TraineeManagementSystem.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Trainee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private int id;
+	private String name;
+	private String location;
+	private String domain;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+}
